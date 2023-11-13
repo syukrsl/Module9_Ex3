51c3c617e7f6a324056801406f78c29b0142c68a
syukrsl <134099202+syukrsl@users.noreply.github.com>
2023-11-13 15:37:08 +0800

    Add CORS

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require("express");
+const cors = require('cors')
 require('dotenv').config();
-const dbConnect = require('./dbConnect');
+const dbConnect = require("./dbConnect");
 dbConnect.connectMySQL();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-let postRoutes = require('./routes/postRoutes');
-app.use('/api/posts', postRoutes);
-
-app.listen(port, () => {
-      console.log("Listening on port ", port);
+let postRoutes = require('./routes/postRoutes')
+app.use('/api/posts', postRoutes)
+app.listen(port,()=>{
+  console.log("Listening on port ", port);
 });
