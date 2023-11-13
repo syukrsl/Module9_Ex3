2b585b9450308ed2aebbbe9462cc55daf01272d9
syukrsl <134099202+syukrsl@users.noreply.github.com>
2023-11-13 14:45:10 +0800

    Add Routes file to Posts

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ dbConnect.connectMySQL();
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+let postRoutes = require('./routes/postRoutes');
+app.use('/api/posts', postRoutes);
+
 app.listen(port, () => {
       console.log("Listening on port ", port);
 });
