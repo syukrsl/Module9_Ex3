da5974ad168623879409cb7f207c21c78789ef94
syukrsl <134099202+syukrsl@users.noreply.github.com>
2023-11-13 11:40:11 +0800

    Add Database Connection file

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+require('dotenv').config();
+const dbConnect = require('./dbConnect');
+dbConnect.connectMySQL();
+
 const app = express();
 const port = process.env.PORT || 8080;
 
